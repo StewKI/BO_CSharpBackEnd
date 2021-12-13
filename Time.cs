1322be0ba508c3fc024037fb5025a3882c25ab72
StewKI <andrijastev4@gmail.com>
2021-12-13 22:36:37 +0100

    Handled exceptions for setters; Implemented "Intersect" method in "Time.cs" and added scheme of intersecting as "intersect.png";

--- Time.cs
+++ Time.cs
@@ -76,19 +76,62 @@ public class Interval{
   public DateTime GetEndTime() => endTime;
   public TimeSpan GetDuration() => endTime-startTime;
 
-  public void SetStartTime(DateTime sTime) => startTime = sTime; // TODO error if after endT
-  public void SetEndTime(DateTime eTime) => endTime = eTime; // TODO error if before startT
-  public void SetDuration(TimeSpan dur) => endTime = startTime+dur;
+  public void SetStartTime(DateTime sTime) {
+    if(sTime >= endTime) 
+      throw new Exception("Error! New StartTime is AFTER current EndTime!");
+    else startTime = sTime;
+  }
+  public void SetEndTime(DateTime eTime) {
+    if(eTime <= startTime) 
+      throw new Exception("Error! New EndTime is BEFORE current StartTime!");
+    else endTime = eTime;
+  }
+  public void SetDuration(TimeSpan dur) {
+    if(dur < new TimeSpan(0))
+      throw new Exception("Error! New Duration is NEGATIVE!");
+    else endTime = startTime+dur;
+  }
 
   public Interval() { startTime = DateTime.Now; endTime = DateTime.Now; }
-  public Interval(DateTime time) { startTime = time; endTime = time; }
-  public Interval(DateTime sTime, DateTime eTime) { startTime = sTime; endTime = eTime; } // TODO same as above
-  public Interval(DateTime sTime, TimeSpan dur) { startTime = sTime; endTime = startTime+dur; }
+  public Interval(DateTime time) { SetStartTime(time); SetEndTime(time); }
+  public Interval(DateTime sTime, DateTime eTime) { SetStartTime(sTime); SetEndTime(eTime); }
+  public Interval(DateTime sTime, TimeSpan dur) { SetStartTime(sTime); SetDuration(dur); }
+  public Interval(Interval toCopy) { SetStartTime(toCopy.GetStartTime()); SetEndTime(toCopy.GetEndTime()); }
 
   public bool NoDuration() => endTime-startTime == new TimeSpan(0);
 
   public Interval? Intersect(Interval other){
-    return null;
-    //TODO implement...
+    if(GetStartTime() < other.GetStartTime()){
+      if(GetEndTime() < other.GetStartTime()){
+        //1st
+        return null;
+      }
+      else{
+        if(GetEndTime() < other.GetEndTime()){
+          //2nd
+          return new Interval(other.GetStartTime(), GetEndTime());
+        }
+        else{
+          //3rd
+          return new Interval(other);
+        }
+      }
+    }
+    else{
+      if(other.GetEndTime() < GetStartTime()){
+        //4rd
+        return null;
+      }
+      else{
+        if(other.GetEndTime() < GetEndTime()){
+          //5th
+          return new Interval(GetStartTime(), other.GetEndTime());
+        }
+        else{
+          //6th
+          return new Interval(GetStartTime(), GetEndTime());
+        }
+      }
+    }
   }
 }
