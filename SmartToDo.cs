fd227b9a166b14892bf247635d98e89a69d8f712
StewKI <andrijastev4@gmail.com>
2021-12-14 00:02:08 +0100

    Added daytime schedule in SmartToDo.cs and added method ConstructDT in Time.cs

--- SmartToDo.cs
+++ SmartToDo.cs
@@ -1,6 +1,16 @@
 
+
 public class SmartToDo{
   List<Task> tasks = new List<Task>();
 
+  Interval Morning   = new Interval(new DateTime(1,1,1, 7,0,0), new DateTime(1,1,1,12,0,0));
+  Interval Afternoon = new Interval(new DateTime(1,1,1,12,0,0), new DateTime(1,1,1,17,0,0)); 
+  Interval Evening   = new Interval(new DateTime(1,1,1,17,0,0), new DateTime(1,1,1,22,0,0));
+  //Default: Morning(7-12), Afternoon(12-17), Evening(17-22)
 
+
+
+
+  public void AddTask(Task newTask){
+  }
 }
--- Time.cs
+++ Time.cs
@@ -1,73 +1,3 @@
-/* OVAJ KOD NE VALJA - OVERCOMPLICATED
-
-public abstract class aTime{
-
-  public aTime(){
-  }
-
-  public abstract DateTime GetStartTime();
-  public abstract DateTime? GetEndTime();
-
-  public abstract aTime? Intersect(aTime other);
-
-  public static bool Isti(DateTime A, DateTime B){
-    if(A.Date.Equals(B.Date) &&
-       A.Hour == B.Hour &&
-       A.Minute == B.Minute && A.)
-  }
-}
-
-public class Event: aTime{
-  DateTime time;
-
-  public Event(DateTime time){
-    this.time = time;
-  }
-
-  public override DateTime GetStartTime(){
-    return time;
-  }
-
-  public override DateTime? GetEndTime()
-  {
-    return null;
-  }
-
-  public override aTime? Intersect(aTime other)
-  {
-    if(other.GetType().ToString() == "Event"){
-
-    }
-    else if(other.GetType().ToString() == "Interval"){
-
-    }
-  }
-}
-
-public class Interval : aTime{
-  DateTime startTime, endTime;
-
-  public Interval(DateTime startTime, DateTime endTime){
-    this.startTime = startTime;
-    this.endTime = endTime;
-  }
-
-  public override DateTime GetStartTime(){
-    return startTime;
-  }
-
-  public override DateTime? GetEndTime()
-  {
-    return endTime;
-  }
-
-  public override aTime? Intersect(aTime other)
-  {
-    throw new NotImplementedException();
-  }
-}
-OVERCOMPLICATED*/
-
 
 public class Interval{
   DateTime startTime, endTime;
@@ -133,5 +63,9 @@ public class Interval{
         }
       }
     }
+  }
+
+  public static DateTime ConstructDT(DateTime dateFrom, DateTime timeFrom){
+    return new DateTime(dateFrom.Year, dateFrom.Month, dateFrom.Day, timeFrom.Hour, timeFrom.Minute, timeFrom.Second);
   }
 }
