5319c8f4da6fbf24b2765b216a5276b1b2b9e135
StewKI <andrijastev4@gmail.com>
2021-12-20 11:24:40 +0100

    runned second test

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿
-SmartToDo App = new SmartToDo();
+SmartToDo App = new SmartToDo(10);
 
 App.AddTask(new Task("1","", new Interval(DateTime.Today, 10,0,11,0)));
 App.AddTask(new Task("2","", new Interval(DateTime.Today, 12,0,13,0)));
 App.AddTask(new Task("3","", new Interval(DateTime.Today, 14,0,15,0)));
 
-var a = App.newTaskCheck(new Task("nt1","", new Interval(DateTime.Today, 12,30, 14,30)));
+var a = App.newTaskCheck(new Task("nt1","", new Interval(DateTime.Today, 12,00, 13,5)), new Interval(DateTime.Today, 1,00, 22,00));
 
 foreach(var i in a){
-  System.Console.WriteLine(i);
+  System.Console.WriteLine("TEST* " + i);
 }
